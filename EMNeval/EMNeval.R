42af601be02923b3711c16a2b42170c2ee124fb5
panda <58083016+mysfxh@users.noreply.github.com>
2026-05-08 21:45:39 +0800

    Clean up EMNeval.R by removing blank lines
    Removed unnecessary blank lines and improved code formatting.

--- EMNeval/EMNeval.R
+++ EMNeval/EMNeval.R
@@ -17,10 +17,6 @@ setwd("/home/xiongh/Biomod2//Maxent/")
 length(which(!is.na(values(subset(env, 1)))))
 sapply(1:nlayers(env), function(i) sum(is.na(values(env[[i]]))))
 bg <-dismo::randomPoints(env[[1]],n=10000)%>%as.data.frame()
-
-
-
 enmeval_results2 <- ENMevaluate(occ, env, bg = bg[,1:2], tune.args = list(fc = c("L","Q","H","LQ","LQH", "LQHP","LQHPT"), rm =  c(0.1,seq(0.5,5,0.5))), partitions = "randomkfold",algorithm = "maxent.jar",categoricals = names(env)[c(10)],clamp = TRUE)
-
 
 write.csv(enmeval_results2@results,"/home/xiongh/Biomod2/new/result_1/enmeval/enm_2914new.csv")
